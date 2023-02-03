08e4345ec88c61b1a7394732b1f53aa617ae98b8
Pedrotr45 <ph6878694@gmail.com>
2023-02-03 16:05:51 -0300

    site institucional

---
+++ form.php
@@ -0,0 +1,20 @@
+<?php
+
+ if(isset($_POST['nome'])) {
+
+    //print_r('nome:'.$_POST['nome']);
+   // print_r('<br>');
+   // print_r('email:'.$_POST['email']);
+   // print_r('<br>');
+    //print_r('telefone:'.$_POST['telefone']);
+
+    include_once('conexao.php');
+
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $telefone = $_POST['telefone'];
+
+    $result = mysqli_query($conexao,"INSERT INTO user(nome,email,telefone) VALUES('$nome','$email','$telefone')");
+ }
+
+?>
